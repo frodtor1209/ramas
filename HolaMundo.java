02d7f2609d87f69f44dc610b08f5d569200c467d
frodtor1209 <frodtor427@iesalmudeyne.es>
2024-10-16 09:29:57 +0200

    Se ha cambiado el codigo de color para uqe  lo muestre en amarillo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,9 @@ public class HolaMundo {
     public static void main(String[] args) {
         String verde = "\033[32m";
         String rojo = "\033[31m";
+        String amarillo = "\033[33m";
         System.out.println(rojo+"Hola Mundo en rojo");
         System.out.println(verde+"Hola Mundo en verde");
+        System.out.println(amarillo+"Hola Mundo en amarillo");
     }
 }   
