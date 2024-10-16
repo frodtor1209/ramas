6aedf53d0a8e5416a8f4be5d913d8abdd7620ef6
frodtor1209 <frodtor427@iesalmudeyne.es>
2024-10-16 08:46:29 +0200

    Version del fichero con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,7 @@
-
-class HolaMundo {
+public class HolaMundo {
     public static void main(String[] args) {
+        String rojo = "\033[32m";
+        String verde = "\033[33m";
         System.out.println("Hola Mundo");
     }
 }   
